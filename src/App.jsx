1aa5507a1db836ef130a3a84a1dd321df993e8eb
Victor <aue228lublupacanov@gmail.com>
2024-02-02 13:23:03 +0200

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,68 @@
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getNotes, createNote, updateNote } from "./requests"
+
+
+
+
 const App = () => {
+  const queryClient = useQueryClient()
+  // const [counter, counterDispatch] = useReducer(counterReducer, 0)
+
+  const newNoteMutation = useMutation({
+    mutationFn: createNote,
+    onSuccess: (newNote) => {
+      const notes = queryClient.getQueriesData('notes')
+      queryClient.setQueryData('notes', notes.concat(newNote))
+    }
+  })
+
+  const updateNoteMutation = useMutation(updateNote, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('notes')
+    },
+  })
+
+
   const addNote = async (event) => {
-    event.preventDefault()
+    // event.preventDefault()
     const content = event.target.note.value
     event.target.note.value = ''
     console.log(content)
+    newNoteMutation.mutate({ content, important: true })
   }
 
   const toggleImportance = (note) => {
-    console.log('toggle importance of', note.id)
+    updateNoteMutation.mutate({ ...note, important: !note.important })
   }
 
-  const notes = []
+  const result = useQuery({
+    queryKey: ['notes'],
+    queryFn: getNotes,
+    refetchOnWindowFocus: false
+  })
+  console.log(JSON.parse(JSON.stringify(result)))
 
-  return(
+  if (result.isLoading) {
+    return <div>loading data..</div>
+  }
+  const notes = result.data
+
+  return (
     <div>
       <h2>Notes app</h2>
       <form onSubmit={addNote}>
         <input name="note" />
         <button type="submit">add</button>
       </form>
-      {notes.map(note =>
-        <li key={note.id} onClick={() => toggleImportance(note)}>
-          {note.content} 
-          <strong> {note.important ? 'important' : ''}</strong>
-        </li>
-      )}
-    </div>
+      {
+        notes.map(note =>
+          <li key={note.id} onClick={() => toggleImportance(note)}>
+            {note.content}
+            <strong> {note.important ? 'important' : ''}</strong>
+          </li>
+        )
+      }
+    </div >
   )
 }
 
